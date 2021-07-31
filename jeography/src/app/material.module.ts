205d04f680e3a2b30039d4e74112e220b3406b4f
namaee <stephen-999@live.nl>
2021-07-31 15:50:20 +0200

    commit idk

---
+++ jeography/src/app/material.module.ts
@@ -0,0 +1,75 @@
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+@NgModule({
+  imports: [
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatMenuModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatCheckboxModule,
+  ],
+  exports: [
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatInputModule,
+    MatMenuModule,
+    MatChipsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatCheckboxModule,
+  ],
+})
+export class MaterialModule {}
