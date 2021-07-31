a7bbbee97af1238448565781aadee0108c558285
namaee <stephen-999@live.nl>
2021-07-31 15:51:20 +0200

    commit idk 2

--- jeography/src/app/app.module.ts
+++ jeography/src/app/app.module.ts
@@ -4,16 +4,20 @@ import { InlineSVGModule } from 'ng-inline-svg';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LegendComponent } from './legend/legend.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    LegendComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    InlineSVGModule.forRoot()
+    InlineSVGModule.forRoot(),
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- jeography/src/app/map/map.component.ts
+++ jeography/src/app/map/map.component.ts
@@ -1,21 +1,31 @@
 import { R3TargetBinder } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
   public map = '../../assets/japan.svg';
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+        .body.style.backgroundColor = '#2b2b2b';
+}
+
   public test(event: MouseEvent){
     let path = event.target as SVGPathElement
-    console.log(path.getAttribute('title'));
+    if (path.hasAttribute('title')) {
+      console.log(path.getAttribute('title'));
+
+    } else {
+      console.log("NO PREFECTURE");
+    }
     
   }
 }
