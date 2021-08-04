bcfa81db476de52777c9163677000b75f3616bea
namaee <stephen-999@live.nl>
2021-08-04 16:11:48 +0200

    view dynamic values

--- jeography/src/app/map/map.service.ts
+++ jeography/src/app/map/map.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { prefectures } from '../data';
+import { prefectures, prefecturesData } from '../data';
 import { Prefecture } from '../quiz/quiz';
 
 @Injectable()
 export class MapService {
   public hover: BehaviorSubject<string> = new BehaviorSubject<string>(null);
   public prefectures = prefectures
+  public prefecturesData = prefecturesData
+  public activePrefectureData: {name: string, capital: string, region: string, island: string, area: number, population: number, flower: string}; 
   public activePrefecture: Prefecture;
 
   constructor() {
@@ -16,6 +18,9 @@ export class MapService {
   public setActivePrefecture(pref: string) {
     this.activePrefecture = this.prefectures.find((prefecture) => {
       return prefecture.name == pref;      
+    })
+    this.activePrefectureData = this.prefecturesData.find((prefecture) => {
+      return prefecture.name == this.activePrefecture.macron;      
     })
   }
 }
