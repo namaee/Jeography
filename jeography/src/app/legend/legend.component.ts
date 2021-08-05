f3b5137191969136fa037aa1fd25b1f86b02c37a
namaee <stephen-999@live.nl>
2021-08-05 22:34:11 +0200

    added rankings

--- jeography/src/app/legend/legend.component.ts
+++ jeography/src/app/legend/legend.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { prefectures } from '../data';
+import { prefectures, prefecturesData } from '../data';
 import { MapService } from '../map/map.service';
 
 @Component({
@@ -9,6 +9,7 @@ import { MapService } from '../map/map.service';
 })
 export class LegendComponent implements OnInit {
   public prefectures = prefectures;
+  public prefecturesData = prefecturesData;
 
   constructor(public mapService: MapService) {
   
@@ -16,6 +17,30 @@ export class LegendComponent implements OnInit {
 
   ngOnInit(): void {
 
+  }
+
+  public getPopRank(pref: string) {
+    const sorted = this.prefecturesData.slice().sort((a, b) => a.population < b.population ? 1 : -1)
+    return sorted.findIndex((prefecture) => prefecture.name == pref) + 1
+  }
+  
+  public getAreaRank(pref: string) {
+    const sorted = this.prefecturesData.slice().sort((a, b) => a.area < b.area ? 1 : -1)
+    return sorted.findIndex((prefecture) => prefecture.name == pref) + 1
+  }
+
+  public numberRankSuffix(rank: number) {
+    let ranking = rank.toString().slice(-1)
+    let numberRanking = parseInt(ranking);
+    if (numberRanking == 1) {
+      return "st"
+    } else if (numberRanking == 2) {
+      return "nd"
+    } else if (numberRanking == 3) {
+      return "rd"
+    } else {
+      return "th"
+    }
   }
 
   public formatKM(num) {
