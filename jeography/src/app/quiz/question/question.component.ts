4a5f5a9f9aeb57cccc43f7df1039b0e03015e154
namaee <stephen-999@live.nl>
2021-08-06 22:39:18 +0200

    select/deselect all

--- jeography/src/app/quiz/question/question.component.ts
+++ jeography/src/app/quiz/question/question.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit, ViewChild, ViewChildren } from '@angular/core';
 import { QuizService } from '../quiz.service';
 import { Subscription } from 'rxjs';
 import { MatCheckboxChange } from '@angular/material/checkbox';
 import { Mode } from '../quiz';
 import { MapService } from 'src/app/map/map.service';
 import { prefectures, citiesSvg } from '../../data';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatListOption } from '@angular/material/list';
 
 @Component({
   selector: 'app-quiz-question',
@@ -16,13 +18,19 @@ export class QuestionComponent implements OnInit, OnDestroy {
   public pickPanelExpanded: boolean = false;
   public prefectures = prefectures;
   public citiesSvg = citiesSvg;
-  constructor(public qs: QuizService, public ms: MapService) { }
+  public selections: {checked: boolean, name: string}[] = [];
+  
+  @ViewChildren('sel') sel;
+  constructor(public qs: QuizService, public ms: MapService, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.subscriptions.add(
       this.qs.quizControl.subscribe((msg: string) => {
       })
     )  
+    citiesSvg.forEach((city) => {
+      this.selections.push({checked: false, name: city.title})
+    })
   }
 
   public startQuiz() {
@@ -31,7 +39,42 @@ export class QuestionComponent implements OnInit, OnDestroy {
       case Mode.CIT: this.qs.startQuiz(Mode.CIT); break;
 
     }
+  }
+
+  public selectAll() {
+    this.sel._results.forEach((selObj) => {
+      selObj._checked = true;
+    })
+  }
+  public deselectAll() {
+    this.sel._results.forEach((selObj) => {
+      selObj._checked = false;
+    })
+  }
+  
+  
+  public selectAllC() {
     
+  }
+  public deselectAllC() {
+    
+  }
+
+  public fillSelect(name: string) {
+    this.selections.forEach((selection) => {
+      if (selection.name == name) {
+        return selection.checked;
+      }
+    })
+  }
+
+  public checkSelect(name: string) {
+    this.selections.forEach((selection) => {
+      if (selection.name == name) {
+        selection.checked = !selection.checked
+      }
+    })
+    console.log(this.sel._results);    
   }
 
   public togglePickPanel(){ 
